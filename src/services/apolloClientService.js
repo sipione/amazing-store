4f748cb1d0701fc30fc1f4b8c54db469dc457c1e
sipione <ricardo@sipione.tech>
2022-06-23 10:20:58 -0300

    apollo client changes

--- src/services/apolloClientService.js
+++ src/services/apolloClientService.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 export const Client = new ApolloClient({
-    //uri: "https://amazing-store-ricardo-sipione.herokuapp.com",
-    uri: "http://localhost:4000",
+    uri: "https://amazing-store-ricardo-sipione.herokuapp.com",
+    //uri: "http://localhost:4000",
     cache: new InMemoryCache()
 })
